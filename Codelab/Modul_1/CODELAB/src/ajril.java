ca51cad19e7e01087db826bdb2e78ab5e1e24655
azril <thskaidono@gmail.com>
2025-03-06 21:53:33 +0700

    hi gtg

---
+++ Codelab/Modul_1/CODELAB/src/ajril.java
@@ -0,0 +1,39 @@
+package Codelab.Modul_1.CODELAB.src;
+
+
+import java.util.Scanner;
+import java.time.LocalDate;
+
+public class  ajril {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan Nama: ");
+        String nama = scanner.nextLine();
+
+        System.out.print("Masukkan Jenis Kelamin (L/P): ");
+        char jenisKelaminChar = scanner.next().charAt(0);
+
+        System.out.print("Masukkan Tahun Lahir: ");
+        int tahunLahir = scanner.nextInt();
+
+        scanner.close();
+
+        String jenisKelamin;
+        if (jenisKelaminChar == 'L' || jenisKelaminChar == 'l') {
+            jenisKelamin = "Laki-laki";
+        } else if (jenisKelaminChar == 'P' || jenisKelaminChar == 'p') {
+            jenisKelamin = "Perempuan";
+        } else {
+            jenisKelamin = "Tidak diketahui";
+        }
+
+        int tahunSekarang = LocalDate.now().getYear();
+        int umur = tahunSekarang - tahunLahir;
+
+        System.out.println("\nData Diri Pengguna:");
+        System.out.println("Nama: " + nama);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Umur: " + umur + " tahun");
+    }
+}
