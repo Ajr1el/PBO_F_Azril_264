0828fc29fc7d8e86e162f0a974a581e07b0dbfbe
azril <thskaidono@gmail.com>
2025-03-07 05:22:13 +0700

    perbaikan

--- Codelab/Modul_1/Tugas_1/src/ajril.java
+++ Codelab/Modul_1/Tugas_1/src/ajril.java
@@ -1,18 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 package Codelab.Modul_1.TUGAS_1;
 
 import java.util.Scanner;
 
-public class Main {
+public class ajril {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Pilih login:");
         System.out.println("1. Admin");
         System.out.println("2. Mahasiswa");
-        System.out.print("Masukkan pilihan: ");
-        int pilihan = scanner.nextInt();
+        System.out.print("Masukkan pilihan:");int pilihan = scanner.nextInt();
         scanner.nextLine();
 
         if (pilihan == 1) {
@@ -21,13 +18,10 @@ public class Main {
             System.out.print("Masukkan password: ");
             String password = scanner.nextLine();
 
-            if (username.startsWith("Admin264") && password.startsWith("Password264")) {
-                String nimAkhir = username.substring(5);
-                if (password.equals("Password" + nimAkhir)) {
-                    System.out.println("Login Admin berhasil!");
-                } else {
-                    System.out.println("Login gagal! Username atau password salah.");
-                }
+            String nimAkhir = username.length() > 5 ? username.substring(5) : "";
+
+            if (username.startsWith("Admin264") && password.equals("Password" + nimAkhir)) {
+                System.out.println("Login Admin berhasil!");
             } else {
                 System.out.println("Login gagal! Username atau password salah.");
             }
@@ -37,7 +31,7 @@ public class Main {
             System.out.print("Masukkan NIM: ");
             String nim = scanner.nextLine();
 
-            if (nama.equals("Azril Maulana Azhar") && nim.equals("202410370110264")) {
+            if ("Azril Maulana Azhar".equals(nama) && "202410370110264".equals(nim)) {
                 System.out.println("Login Mahasiswa berhasil!");
                 System.out.println("Nama: " + nama);
                 System.out.println("NIM: " + nim);
